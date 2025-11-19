c9cc2d25cfc81870e55e5ce60cd4645cb44882e7
VUNDELA VENKATA CHANDRAHAS REDDY <vvchandrahasreddy7@gmail.com>
2025-11-19 10:27:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.java
@@ -0,0 +1,33 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+};
+*/
+
+class Solution {
+    public int cycleStart(Node head) {
+        // code here
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                fast=fast.next;
+                slow=slow.next;
+                }
+                return slow.data;
+            }
+        }
+        return -1;
+    }
+}
